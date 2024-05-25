734afbdf38d25036df7f799f8e35a57e242df532
unknown <dn06023@naver.com>
2024-05-25 15:43:44 +0900

    feat : 예약 정보를 저장하는 클래스
    String date : 예약 날짜 정보

---
+++ src/main/java/com/example/demo/DTO/ReservationInfo.java
@@ -0,0 +1,13 @@
+package com.example.demo.DTO;
+
+public class ReservationInfo {
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
