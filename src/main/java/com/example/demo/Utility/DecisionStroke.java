5915c358ac370899435a2dd974d313405d85ba64
unknown <dn06023@naver.com>
2024-05-31 16:16:12 +0900

    feat : DecisionStroke 클래스 추가

--- src/main/java/com/example/demo/Utility/DecisionStroke.java
+++ src/main/java/com/example/demo/Utility/DecisionStroke.java
@@ -1,2 +1,14 @@
-package com.example.demo.Utility;public class DecisionStroke {
+package com.example.demo.Utility;
+
+import com.example.demo.DTO.UserInfo;
+
+public class DecisionStroke {
+    public UserInfo startDecision(){
+        UserInfo userInfo = null;
+        return userInfo;
+    }
+
+    public void updateResult(){
+
+    }
 }
