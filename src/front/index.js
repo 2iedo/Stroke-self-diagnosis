f8814716d4aa85d964f268ef844cccbf1e3e7f4a
seung365 <bdh3659@naver.com>
2024-05-31 15:26:57 +0900

    feat: hospital page 이동 추가

--- src/front/index.js
+++ src/front/index.js
@@ -1,27 +1,45 @@
-import Checklist from "./component/Checklist";
-import Result from "./component/Result";
+import Checklist from "./component/Checklist"
+import Result from "./component/Result"
+import Come from "./component/Come"
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.querySelector(".root");
+  const root = document.querySelector(".root")
 
   function render() {
-    const hash = window.location.hash;
-    root.innerHTML = ""; // Clear the root element
+    const hash = window.location.hash.trim() // 해시 값의 공백 제거
+    root.innerHTML = "" // Clear the root element
+
+    console.log("Current hash:", hash) // 해시 값을 콘솔에 출력하여 디버깅
 
     switch (hash) {
       case "":
       case "#":
-        const checklist = new Checklist();
-        root.appendChild(checklist.el);
-        break;
+        {
+          const checklist = new Checklist()
+          checklist.render()
+          root.appendChild(checklist.el)
+        }
+        break
       case "#result":
-        const result = new Result();
-        root.appendChild(result.el);
-        break;
+        {
+          const result = new Result()
+          result.render()
+          root.appendChild(result.el)
+        }
+        break
+      case "#hospital":
+        {
+          const hospital = new Come()
+          hospital.render()
+          root.appendChild(hospital.el)
+        }
+        break
+
       default:
-        root.innerHTML = "<p>The requested page could not be found.</p>";
+        root.innerHTML = "<p>The requested page could not be found.</p>"
     }
   }
-  window.addEventListener("hashchange", render);
-  render();
-});
+
+  window.addEventListener("hashchange", render)
+  render()
+})
