f3705616d55302da72fa460cbfe8b6202b874c6d
seung365 <bdh3659@naver.com>
2024-05-31 15:23:19 +0900

    refactor: Result.js 리팩토링

--- src/front/component/Result.js
+++ src/front/component/Result.js
@@ -1,11 +1,18 @@
-import { Component } from "./Core";
+import { Component } from "./Core"
 
 export default class Result extends Component {
   render() {
+    const formData = JSON.parse(localStorage.getItem("formData"))
     this.el.innerHTML = /* html */ `
       <h1>Result Page</h1>
       <p>This is the result of your self-diagnosis.</p>
-      
-    `;
+      <h1>진단 결과</h1>
+      <p>이름: ${formData.name}</p>
+      <p>나이: ${formData.age}</p>
+      <p>성별: ${formData.gender}</p>
+      <p>증상: ${formData.symptoms.join(", ")}</p>
+      <button type="button" class="reset" onclick="window.location.hash = '#'">다시 진단하기</button>
+      <button type="button" class="reserve" onclick="window.location.hash = '#hospital'">병원 예약</button>
+    `
   }
 }
