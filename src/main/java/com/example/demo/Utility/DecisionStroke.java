e5347a73f4eff4b2f3f6125d01dc287452dd0174
suhyeon <nucc1186@gamil.com>
2024-06-02 15:20:32 +0900

    feat: userInfo.StrokePercent to frontend

--- src/main/java/com/example/demo/Utility/DecisionStroke.java
+++ src/main/java/com/example/demo/Utility/DecisionStroke.java
@@ -1,11 +1,20 @@
 package com.example.demo.Utility;
 
 import com.example.demo.DTO.UserInfo;
+import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/DecisionStroke")
 public class DecisionStroke {
-    public UserInfo startDecision(){
-        UserInfo userInfo = null;
-        return userInfo;
+
+    @CrossOrigin(origins = "http://localhost:1234")
+    @GetMapping
+    public String startDecision(){
+        Gson gson = new Gson();
+        UserInfo userInfo = null; // 이부분 읽어 오기
+        String userInfoJson = gson.toJson(userInfo.getStrokePercent());
+        return userInfoJson;
     }
 
     public void updateResult(){
