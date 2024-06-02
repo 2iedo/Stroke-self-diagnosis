912c8d1ca9d0e130e5dc43493d64e33496d54325
unknown <dn06023@naver.com>
2024-06-02 14:09:09 +0900

    style : 리턴값 형태 수정 예측값 및 실제 값 출력에서 실제 값만 리턴하도록 수정

--- predictionApiLauncher.py
+++ predictionApiLauncher.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 model = joblib.load('stroke_model.pkl')
 
 @app.route('/predict', methods=['POST'])
-@app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     input_data = np.array([[data[field] for field in data]])
     prediction = model.predict(input_data) #뇌졸중 여부 (데이터 기반)
     prediction_proba = model.predict_proba(input_data)[:, 1]  # 뇌졸중 확률
-    return jsonify({'prediction': prediction.tolist(), 'probability': prediction_proba.tolist()})
+    print(prediction_proba)
+    return jsonify(prediction_proba.tolist())
 
 
 if __name__ == '__main__':
