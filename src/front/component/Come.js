294e2b72083ae37a58979340a23f51682ff6bf2d
seung365 <bdh3659@naver.com>
2024-06-22 15:03:00 +0900

    feat: 병원 리스트 출력

--- src/front/component/Come.js
+++ src/front/component/Come.js
@@ -5,36 +5,45 @@ export default class Come extends Component {
     const formData = JSON.parse(localStorage.getItem("formData"))
     this.el.innerHTML = /* html */ `
     <h1> 🏥 병원 추천 드립니다</h1>
-    <ul>
-      <li class="info">      
-      <p>병원 이름 : 경북 신경외과</p>
-      <p>병원 위치 : 산격동</p>
-      <p>병원 여는시간 : 9</p>
-      <p>병원 닫는시간 : 11</p>
-      </li>
-    <li class="info">   
-      <p>병원 이름 : 강남 신경외과</p>
-      <p>병원 위치 : 침산동</p>
-      <p>병원 여는시간 : 9</p>
-      <p>병원 닫는시간 : 11</p>
-    </li>
-    <li class="info">     
-      <p>병원 이름 : 대구 신경외과</p>
-      <p>병원 위치 : 복현동</p>
-      <p>병원 여는시간 : 9</p>
-      <p>병원 닫는시간 : 11</p>
-    </li>
-    </ul>
-
-    <button type="button" class="reset" onclick="window.location.hash = '#'">홈으로</button>
     `
 
-    const listItems = this.el.querySelectorAll(".info")
-    listItems.forEach((item) => {
-      item.addEventListener("click", () => {
-        alert("병원을 선택하셨습니다.")
-        window.location.hash = "#reserve"
-      })
+    fetch("http://localhost:8080/api/Search/hospitalList", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Origin: "http://localhost:1234",
+      },
     })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data)
+        Array.from(data).forEach((hospital) => {
+          this.el.innerHTML += /* html */ `
+            <ul>
+              <li class="info">
+                <p>병원 이름: ${hospital.hospitalName}</p>
+                <p>병원 위치: ${hospital.address}</p>
+                <p>병원 여는시간: ${hospital.hospitalOpening}</p>
+                <p>병원 닫는시간: ${hospital.hospitalClosing}</p>
+              </li>
+            </ul>
+          `
+        })
+        this.el.innerHTML += /* html */ `
+        <button type="button" class="reset" onclick="window.location.hash = '#'">홈으로</button>`
+        const listItems = this.el.querySelectorAll(".info")
+        listItems.forEach((item) => {
+          item.addEventListener("click", () => {
+            alert("병원을 선택하셨습니다.")
+            window.location.hash = "#reserve"
+          })
+        })
+      })
+      .catch((error) => console.error("Error fetching data:", error))
   }
 }
